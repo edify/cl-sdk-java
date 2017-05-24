7a43ef023d29920dc720b5e48c6c1cb6c44de0f5
diugalde <dua.0695@gmail.com>
2017-05-24 04:15:54 -0600

    Add learningObject sdk and gitlab-ci

---
+++ cl-sdk-java-lo/README.md
@@ -0,0 +1,149 @@
+# cl-sdk-java-lo
+
+CL LearningObject SDK
+
+---
+
+This guide gives the user working code examples about how to interact with the CL - LearningObjects micro service.
+
+
+## General usage
+
+1.  Import in gradle
+
+    ```bash
+    compile "org.commonlibrary:cl-sdk-lo:0.0.1"
+    ```
+
+
+2.  First, you need to import the sdk client and create a new instance. In order to obtain a new LearningObject object, you need to provide valid credentials.
+
+    ```groovy
+    import org.commonlibrary.clsdk.lo.LearningObject
+    
+    public static void main(String [] args) {
+        String apiKeyId = "98afcdf62b5ccb73c35b8b2abc061af4"
+        String apiSecretKey = "148760d7fd8e9a205e21eefedda352773dd3c925"
+        ApiKeyCredentials apiKeyCredentials = new ApiKeyCredentials(apiKeyId, apiSecretKey)
+        String baseUrl = "http://localhost:808"
+        String apiUrl = "/api/v1"
+        LearningObject lo = new LearningObject(apiKeyCredentials, baseUrl, apiUrl)
+    }
+    ```
+
+3.  All the LearningObject functions return a Map with the following structure:
+
+    ```groovy
+    [
+        statusCode: int,
+        body: Map<String, Object>
+    ]
+    ```
+
+
+4.  Supported methods:
+
+### Save learningObjective
+
+#### Code
+
+```groovy
+def learningObjective = [
+    name: "New learning objective name",
+    description: "description"
+]
+println lo.saveLearningObjective(learningObjective)
+```
+
+#### Response
+
+```groovy
+[statusCode:200, body:[creationDate:1495619173918, description:description, id:5925566574d4d270c3f5243c, learningObjectiveList:[], modificationDate:1495619173918, name:New learning objective name, new:false]]
+```
+
+
+### Save learningObject
+
+#### Code
+
+```groovy
+def learningObject = [
+    name: "LearningObject name",
+    description: "LO description",
+    type: "EXAM",
+    title: "LO Title",
+    enabled: true,
+    metadata: [
+        coverage: "Learning Object coverage",
+        context: "PRIMARY_EDUCATION",
+        difficulty: "VERY_LOW",
+        endUser: "LEARNERS",
+        language: "English",
+        status: "FINAL",
+        author: "Learning Object author",
+        isbn: "Learning Object ISBN",
+        price: "100",
+        extraMetadata: ["Additional metadata key,Additional metadata value"]
+    ]
+]
+println lo.save(learningObject)
+```
+
+#### Response
+
+```groovy
+[statusCode:200, body:[compoundContent:false, contents:null, creationDate:1495619479975, description:LO description, enabled:true, externalUrl:null, format:null, id:5925579874d4d270c3f5243d, isPublic:true, learningObjectiveList:[], metadata:[author:Learning Object author, context:PRIMARY_EDUCATION, coverage:Learning Object coverage, difficulty:VERY_LOW, endUser:LEARNERS, extraMetadata:[Additional metadata key,Additional metadata value], interactivityDegree:null, isbn:Learning Object ISBN, keywords:null, language:English, price:100.0, status:FINAL, topic:null], modificationDate:1495619479975, name:LearningObject name, new:false, subject:null, title:LO Title, type:EXAM]]
+```
+
+
+### Find learningObject by id
+
+#### Code
+
+```groovy
+def loId = "5925579874d4d270c3f5243d"
+println lo.findById(loId)
+```
+
+#### Response
+
+```groovy
+[statusCode:200, body:[compoundContent:false, contents:[creationDate:1495611846927, id:592539c674d4a0d28c9a8bab, md5:null, mimeType:text/html, modificationDate:1495611847586, new:false, resourcesURL:[], url:/learningObjects/592539c674d4a0d28c9a8baa/contents/592539c674d4a0d28c9a8bab/file/Conclusion.html?refPath=592539c674d4a0d28c9a8baa/], creationDate:1495611846915, description:null, enabled:true, externalUrl:null, format:HTML, id:592539c674d4a0d28c9a8baa, isPublic:true, learningObjectiveList:[], metadata:[author:null, context:ANY, coverage:null, difficulty:ANY, endUser:ANY, extraMetadata:[priority,9, type,INFORMATION], interactivityDegree:ANY, isbn:null, keywords:null, language:English, price:0.0, status:ANY, topic:null], modificationDate:1495611847588, name:Conclusion, new:false, subject:null, title:Conclusion, type:null]]
+```
+
+
+### Store learningObject's contents
+
+#### Code
+
+```groovy
+def contents = [
+    filename: "filename.xml",
+    md5: "5d39af9a571b3166fe88aad88bd043bc",
+    mimeType: "text/xml",
+    base64Content: "PHF1ZXN0ZXN0aW50ZXJvcCB4bWxucz0na+"
+]
+println lo.storeContents("5925579874d4d270c3f5243d", contents)
+```
+
+#### Response
+
+```groovy
+[statusCode:200, body:[md5:null, mimeType:text/xml, url:/learningObjects/5925579874d4d270c3f5243d/contents/5925594474d4d270c3f5243e/file/filename.xml?refPath=5925579874d4d270c3f5243d/]]
+```
+
+
+### Get file's inputStream
+
+#### Code
+
+```groovy
+def learningObject = lo.findById("5925579874d4d270c3f5243d").body
+println lo.getFileInputStream(learObj).text
+```
+
+#### Response
+
+```
+<questestinterop xmlns='k
+```
---
+++ cl-sdk-java-lo/src/main/groovy/org/commonlibrary/clsdk/lo/LearningObject.groovy
@@ -0,0 +1,162 @@
+package org.commonlibrary.clsdk.lo
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import groovy.json.JsonSlurper
+import okhttp3.Headers
+import okhttp3.HttpUrl
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.commonlibrary.clauth.SAuthc1Signer
+import org.commonlibrary.clauth.model.ApiKeyCredentials
+
+/**
+ * Created by diugalde on 20/05/17.
+ */
+class LearningObject {
+
+    private OkHttpClient client
+    private def apiUrl
+    private def baseUrl
+    private ApiKeyCredentials apiKeyCredentials
+    private SAuthc1Signer sAuthc1Signer
+    private static final def NONCE_LENGTH = 15
+    private static final MediaType JSON_MEDIA = MediaType.parse("application/json; charset=utf-8")
+    private ObjectMapper mapper
+    private def jsonSlurper
+
+    LearningObject(ApiKeyCredentials apiKeyCredentials, baseUrl, apiUrl) {
+        client = new OkHttpClient();
+        this.baseUrl = baseUrl
+        this.apiUrl = apiUrl
+        this.apiKeyCredentials = apiKeyCredentials
+        sAuthc1Signer = new SAuthc1Signer()
+        jsonSlurper = new JsonSlurper()
+        mapper = new ObjectMapper()
+        mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+    }
+
+    def saveLearningObjective(learningObjective) {
+        def path = "/learningObjectives"
+        Response response = post(path, learningObjective)
+        return buildMapResponse(response)
+    }
+
+    def save(learningObject) {
+        def path = "/learningObjects"
+        Response response = post(path, learningObject)
+        return buildMapResponse(response)
+    }
+
+    def findById(id) {
+        def path = """/learningObjects/${id}"""
+        Response response = get(path)
+        return buildMapResponse(response)
+    }
+
+    def storeContents(id, contents) {
+        def path = """/learningObjects/${id}/file"""
+        Response response = post(path, contents)
+        return buildMapResponse(response)
+    }
+
+    InputStream getFileInputStream(learningObject) {
+        URI uri = URI.create(learningObject.contents.url)
+        def path = uri.getPath()
+        def queryParams = [refPath: uri.getQuery().split("=")[1]]
+        Response response = get(path, queryParams)
+        return response.body().byteStream()
+    }
+
+    private def get(path, Map<String,String> params = null, apiUrl = this.apiUrl) {
+        def url = """${baseUrl}${apiUrl}${path}"""
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder()
+
+        if (params) {
+            params.each() { name, value -> urlBuilder.addQueryParameter(name, value) }
+        }
+
+        HttpUrl httpUrl = urlBuilder.build()
+        def headers = generateAuthHeaders(httpUrl.toString(), "get", "")
+
+        Request request = new Request.Builder().headers(headers).url(httpUrl).build()
+        Response response = client.newCall(request).execute()
+
+        return response
+    }
+
+    private def put(path, bodyObject, apiUrl = this.apiUrl) {
+        def url = """${baseUrl}${apiUrl}${path}"""
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder()
+
+        def jsonString = "{}"
+        if (bodyObject) {
+            jsonString = mapper.writeValueAsString(bodyObject)
+        }
+
+        HttpUrl httpUrl = urlBuilder.build()
+        def headers = generateAuthHeaders(httpUrl.toString(), "put", jsonString)
+        RequestBody body = RequestBody.create(JSON_MEDIA, jsonString)
+
+        Request request = new Request.Builder().headers(headers).url(httpUrl).put(body).build()
+        Response response = client.newCall(request).execute();
+
+        return response
+    }
+
+    private def post(path, bodyObject, apiUrl = this.apiUrl) {
+        def url = """${baseUrl}${apiUrl}${path}"""
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder()
+
+        def jsonString = "{}"
+        if (bodyObject) {
+            jsonString = mapper.writeValueAsString(bodyObject)
+        }
+
+        HttpUrl httpUrl = urlBuilder.build()
+        def headers = generateAuthHeaders(httpUrl.toString(), "post", jsonString)
+        RequestBody body = RequestBody.create(JSON_MEDIA, jsonString)
+
+        Request request = new Request.Builder().headers(headers).url(httpUrl).post(body).build()
+        Response response = client.newCall(request).execute();
+
+        return response
+    }
+
+    private def delete(path, apiUrl = this.apiUrl) {
+        def url = """${baseUrl}${apiUrl}${path}"""
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder()
+
+        HttpUrl httpUrl = urlBuilder.build()
+        def headers = generateAuthHeaders(httpUrl.toString(), "delete", "")
+
+        Request request = new Request.Builder().headers(headers).url(httpUrl).delete().build()
+        Response response = client.newCall(request).execute()
+
+        return response
+    }
+
+    private Headers generateAuthHeaders(url, method, body) {
+        body = body.replaceAll("[ \n]", "")
+        def headersMap = [:]
+        def date = new Date()
+        def nonce = Utils.generateRandomString(NONCE_LENGTH)
+
+        sAuthc1Signer.sign(headersMap, method, url, body, date, apiKeyCredentials, nonce)
+
+        Headers.Builder headersBuilder = new Headers.Builder()
+        headersMap.each() { name, value -> headersBuilder.add(name, value.toString()) }
+
+        return headersBuilder.build()
+    }
+
+    private def buildMapResponse(Response response) {
+        [
+            statusCode: response.code(),
+            body: response.body().string() ? jsonSlurper.parseText(response.body().string()) : [:]
+        ]
+    }
+}
